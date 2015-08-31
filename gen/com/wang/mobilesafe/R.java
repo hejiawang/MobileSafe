c547e28ce0b60eecbdd5989c9ae6f64af72098e1
hejiawang <952327407@qq.com>
2015-08-31 10:33:59 +0800

    splash page

--- gen/com/wang/mobilesafe/R.java
+++ gen/com/wang/mobilesafe/R.java
@@ -23,20 +23,25 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int shenmatran=0x7f020001;
+        public static final int splashbk=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int iv_splash_logo=0x7f080000;
+        public static final int tv_splash_appname=0x7f080001;
+        public static final int tv_splash_version=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_splash=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
