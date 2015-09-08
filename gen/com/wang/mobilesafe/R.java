99cbed94971cc3c5c07baef934f0e7f80ff7f8d6
hejiawang <952327407@qq.com>
2015-09-08 08:23:01 +0800

    播放报警音乐

--- gen/com/wang/mobilesafe/R.java
+++ gen/com/wang/mobilesafe/R.java
@@ -41,8 +41,8 @@ containing a value of this type.
         public static final int unchecked_text=0x7f010001;
     }
     public static final class color {
-        public static final int gray=0x7f050001;
-        public static final int trans=0x7f050000;
+        public static final int gray=0x7f060001;
+        public static final int trans=0x7f060000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -52,8 +52,8 @@ containing a value of this type.
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f070000;
-        public static final int activity_vertical_margin=0x7f070001;
+        public static final int activity_horizontal_margin=0x7f080000;
+        public static final int activity_vertical_margin=0x7f080001;
     }
     public static final class drawable {
         public static final int app=0x7f020000;
@@ -86,33 +86,33 @@ containing a value of this type.
         public static final int unlock=0x7f02001b;
     }
     public static final class id {
-        public static final int bt_fiest_entry_cancle=0x7f0a000f;
-        public static final int bt_fiest_entry_ok=0x7f0a000e;
-        public static final int bt_normal_entry_cancle=0x7f0a0012;
-        public static final int bt_normal_entry_ok=0x7f0a0011;
-        public static final int cb_setup4_satus=0x7f0a0007;
-        public static final int cb_status=0x7f0a0019;
-        public static final int ed_fiest_entry_pwd=0x7f0a000c;
-        public static final int ed_fiest_entry_pwd_confirm=0x7f0a000d;
-        public static final int ed_normal_entry_pwd=0x7f0a0010;
-        public static final int et_safenumber=0x7f0a0006;
-        public static final int gv_home=0x7f0a0000;
-        public static final int item_change_name=0x7f0a001a;
-        public static final int iv_home_icon=0x7f0a0013;
-        public static final int iv_lostfind_status=0x7f0a0002;
-        public static final int iv_setup2_bind=0x7f0a0005;
-        public static final int iv_splash_logo=0x7f0a0009;
-        public static final int lv_select_contact=0x7f0a0003;
-        public static final int rl_splash=0x7f0a0008;
-        public static final int sv_setting_update=0x7f0a0004;
-        public static final int tv_contact_name=0x7f0a0015;
-        public static final int tv_contact_phone=0x7f0a0016;
-        public static final int tv_home_name=0x7f0a0014;
-        public static final int tv_lostfind_number=0x7f0a0001;
-        public static final int tv_settingview_content=0x7f0a0018;
-        public static final int tv_settingview_title=0x7f0a0017;
-        public static final int tv_splash_appname=0x7f0a000a;
-        public static final int tv_splash_version=0x7f0a000b;
+        public static final int bt_fiest_entry_cancle=0x7f0b000f;
+        public static final int bt_fiest_entry_ok=0x7f0b000e;
+        public static final int bt_normal_entry_cancle=0x7f0b0012;
+        public static final int bt_normal_entry_ok=0x7f0b0011;
+        public static final int cb_setup4_satus=0x7f0b0007;
+        public static final int cb_status=0x7f0b0019;
+        public static final int ed_fiest_entry_pwd=0x7f0b000c;
+        public static final int ed_fiest_entry_pwd_confirm=0x7f0b000d;
+        public static final int ed_normal_entry_pwd=0x7f0b0010;
+        public static final int et_safenumber=0x7f0b0006;
+        public static final int gv_home=0x7f0b0000;
+        public static final int item_change_name=0x7f0b001a;
+        public static final int iv_home_icon=0x7f0b0013;
+        public static final int iv_lostfind_status=0x7f0b0002;
+        public static final int iv_setup2_bind=0x7f0b0005;
+        public static final int iv_splash_logo=0x7f0b0009;
+        public static final int lv_select_contact=0x7f0b0003;
+        public static final int rl_splash=0x7f0b0008;
+        public static final int sv_setting_update=0x7f0b0004;
+        public static final int tv_contact_name=0x7f0b0015;
+        public static final int tv_contact_phone=0x7f0b0016;
+        public static final int tv_home_name=0x7f0b0014;
+        public static final int tv_lostfind_number=0x7f0b0001;
+        public static final int tv_settingview_content=0x7f0b0018;
+        public static final int tv_settingview_title=0x7f0b0017;
+        public static final int tv_splash_appname=0x7f0b000a;
+        public static final int tv_splash_version=0x7f0b000b;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
@@ -131,12 +131,15 @@ containing a value of this type.
         public static final int ui_setting_view=0x7f03000d;
     }
     public static final class menu {
-        public static final int lost_find_menu=0x7f090000;
+        public static final int lost_find_menu=0x7f0a0000;
+    }
+    public static final class raw {
+        public static final int ylzs=0x7f050000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
-        public static final int app_name=0x7f060001;
-        public static final int serverurl=0x7f060000;
+        public static final int action_settings=0x7f070002;
+        public static final int app_name=0x7f070001;
+        public static final int serverurl=0x7f070000;
     }
     public static final class style {
         /** 
@@ -151,14 +154,14 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080007;
-        public static final int button_next_style=0x7f080005;
-        public static final int button_pre_style=0x7f080006;
-        public static final int image_off_style=0x7f080004;
-        public static final int image_on_style=0x7f080003;
-        public static final int image_star_style=0x7f080002;
-        public static final int text_content_style=0x7f080001;
-        public static final int text_title_style=0x7f080000;
+        public static final int AppBaseTheme=0x7f090007;
+        public static final int button_next_style=0x7f090005;
+        public static final int button_pre_style=0x7f090006;
+        public static final int image_off_style=0x7f090004;
+        public static final int image_on_style=0x7f090003;
+        public static final int image_star_style=0x7f090002;
+        public static final int text_content_style=0x7f090001;
+        public static final int text_title_style=0x7f090000;
     }
     public static final class styleable {
         /** Attributes that can be used with a setting_view_style.
--- src/com/wang/mobilesafe/receiver/SmsReceiver.java
+++ src/com/wang/mobilesafe/receiver/SmsReceiver.java
@@ -1,10 +1,13 @@
 package com.wang.mobilesafe.receiver;
 
+import com.wang.mobilesafe.R;
 import com.wang.mobilesafe.engine.GPSInfoProvider;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.text.TextUtils;
@@ -13,17 +16,20 @@ import android.util.Log;
 public class SmsReceiver extends BroadcastReceiver {
 
 	private static final String TAG = "SmsReceiver";
+	private SharedPreferences sp;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
+		sp = context.getSharedPreferences("config", context.MODE_PRIVATE);
 		Object[] pdus = (Object[]) intent.getExtras().get("pdus");
 		for (Object pdu : pdus) {
 
 			SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdu);
 			String sender = smsMessage.getOriginatingAddress();
 			String body = smsMessage.getMessageBody();
-			if ("#*location*#".equals(body)) {
+			if ("#*location*#".equals(body) && sp.getString("safenumber", "").equals(sender)) {
+				
 				Log.i(TAG, "返回手机位置....");
 				String location = GPSInfoProvider.getInstance(context).getLastLocation();
 				if ( !TextUtils.isEmpty(location) ) {
@@ -34,12 +40,18 @@ public class SmsReceiver extends BroadcastReceiver {
 				//这样就收不到短信了
 				abortBroadcast();
 			} else if ("#*alarm*#".equals(body)) {
+				
 				Log.i(TAG, "播放报警音乐....");
+				MediaPlayer mp = MediaPlayer.create(context, R.raw.ylzs);
+				mp.setVolume(1.0f, 1.0f);
+				mp.start();
 				abortBroadcast();
 			} else if ("#*wipedata*#".equals(body)) {
+				
 				Log.i(TAG, "清空数据....");
 				abortBroadcast();
 			} else if ("#*lockscreen*#".equals(body)) {
+				
 				Log.i(TAG, "锁屏....");
 				abortBroadcast();
 			}
