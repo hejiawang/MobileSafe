588d7508d8eab46e637a227c35b59f68b6996b3a
hejiawang <952327407@qq.com>
2015-10-14 09:45:35 +0800

    缓存清理的实现——跳到设置界面进行清理

--- src/com/wang/mobilesafe/CleanCacheActivity.java
+++ src/com/wang/mobilesafe/CleanCacheActivity.java
@@ -4,19 +4,23 @@ import java.lang.reflect.Method;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.IPackageStatsObserver;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageStats;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.PackageStats;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.RemoteException;
 import android.text.format.Formatter;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 /**
@@ -26,48 +30,102 @@ import android.widget.TextView;
  *
  */
 public class CleanCacheActivity extends Activity {
+
+	private static final int SCAN_FINISH = 0;
+	private static final int SCAN_ING = 1;
+
 	private PackageManager pm;
+
 	private LinearLayout ll_container;
+	private TextView tv_clean_cache_status;
+	private ProgressBar pb;
+
 	private Handler handler = new Handler() {
 		public void handleMessage(android.os.Message msg) {
-			MyPackageInfo info = (MyPackageInfo) msg.obj;
 
-			if (info.cachesize > 0) {
-
-				View view = View.inflate(getApplicationContext(),
-						R.layout.list_clean_cache, null);
-				ImageView iv = (ImageView) view.findViewById(R.id.imageView1);
-				TextView tv_name = (TextView) view.findViewById(R.id.textView1);
-				TextView tv_codesize = (TextView) view
-						.findViewById(R.id.textView2);
+			switch (msg.what) {
+			case SCAN_ING:
 
 				try {
-					iv.setImageDrawable(pm.getApplicationInfo(info.packname, 0)
-							.loadIcon(pm));
-					tv_name.setText(pm.getApplicationInfo(info.packname, 0)
-							.loadLabel(pm));
-					tv_codesize.setText("缓存大小:"
-							+ Formatter.formatFileSize(getApplicationContext(),
-									info.cachesize));
+
+					final MyPackageInfo info = (MyPackageInfo) msg.obj;
+
+					tv_clean_cache_status.setText(" 正在扫描: "
+							+ pm.getApplicationInfo(info.packname, 0)
+									.loadLabel(pm));
+					if (info.cachesize > 0) {
+
+						View view = View.inflate(getApplicationContext(),
+								R.layout.list_clean_cache, null);
+						ImageView iv = (ImageView) view
+								.findViewById(R.id.imageView1);
+						TextView tv_name = (TextView) view
+								.findViewById(R.id.textView1);
+						TextView tv_codesize = (TextView) view
+								.findViewById(R.id.textView2);
+
+						iv.setImageDrawable(pm.getApplicationInfo(
+								info.packname, 0).loadIcon(pm));
+						tv_name.setText(pm.getApplicationInfo(info.packname, 0)
+								.loadLabel(pm));
+						tv_codesize.setText("缓存大小:"
+								+ Formatter
+										.formatFileSize(
+												getApplicationContext(),
+												info.cachesize));
+
+						view.setClickable(true);
+						view.setBackgroundResource(R.drawable.home_selector);
+						view.setOnClickListener(new OnClickListener() {
+
+							@Override
+							public void onClick(View v) {
+
+								String packname = info.packname;
+								// 开启设置界面，让用户自己清理缓存...
+								Intent intent = new Intent();
+								intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
+								intent.setData(Uri.parse("package:" + packname));
+								startActivity(intent);
+
+							}
+						});
+
+						ll_container.addView(view, 0);
+					}
 				} catch (NameNotFoundException e) {
 					e.printStackTrace();
 				}
-				ll_container.addView(view, 0);
+				break;
+
+			case SCAN_FINISH:
+
+				tv_clean_cache_status.setText(" 扫描完毕 ");
+				break;
 			}
+
 		};
 	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_clean_cache);
+
 		ll_container = (LinearLayout) findViewById(R.id.ll_container);
+		tv_clean_cache_status = (TextView) findViewById(R.id.tv_clean_cache_status);
+		pb = (ProgressBar) findViewById(R.id.progressBar1);
 
 		pm = getPackageManager();
 		new Thread() {
 			public void run() {
+
 				List<PackageInfo> infos = pm.getInstalledPackages(0);
+				pb.setMax(infos.size());
+				int total = 0;
 				for (PackageInfo info : infos) {
+
 					String packname = info.packageName;
 					getPackageSize(packname);
 					try {
@@ -75,12 +133,19 @@ public class CleanCacheActivity extends Activity {
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
+					total++;
+					pb.setProgress(total);
 				}
+
+				Message msg = Message.obtain();
+				msg.what = SCAN_FINISH;
+				handler.sendMessage(msg);
 			};
 		}.start();
 	}
 
 	private void getPackageSize(String packname) {
+
 		try {
 			Method method = PackageManager.class.getMethod(
 					"getPackageSizeInfo", new Class[] { String.class,
@@ -94,6 +159,7 @@ public class CleanCacheActivity extends Activity {
 	}
 
 	private class MyObserver extends IPackageStatsObserver.Stub {
+
 		private String packname;
 
 		public MyObserver(String packname) {
@@ -103,15 +169,18 @@ public class CleanCacheActivity extends Activity {
 		@Override
 		public void onGetStatsCompleted(PackageStats pStats, boolean succeeded)
 				throws RemoteException {
+
 			long cachesize = pStats.cacheSize;
 			long codesize = pStats.codeSize;
-			//long datasize = pStats.dataSize;
+			// long datasize = pStats.dataSize;
 
 			MyPackageInfo mypackinfo = new MyPackageInfo();
 			mypackinfo.codesize = codesize;
 			mypackinfo.cachesize = cachesize;
 			mypackinfo.packname = packname;
+
 			Message msg = Message.obtain();
+			msg.what = SCAN_ING;
 			msg.obj = mypackinfo;
 			handler.sendMessage(msg);
 		}
@@ -119,6 +188,7 @@ public class CleanCacheActivity extends Activity {
 	}
 
 	class MyPackageInfo {
+
 		long codesize;
 		long cachesize;
 		String packname;
